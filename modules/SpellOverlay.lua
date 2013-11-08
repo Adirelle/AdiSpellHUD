bfc8a05b728d65dcf31aa9e2fa117bdf4cee35dc
Adirelle <adirelle@gmail.com>
2013-11-08 20:36:24 +0100

    Licensed with GPL v3.

--- modules/SpellOverlay.lua
+++ modules/SpellOverlay.lua
@@ -2,6 +2,21 @@
 AdiSpellHUD - Spell overlay customization and spell state HUD.
 Copyright 2011-2013 Adirelle (adirelle@gmail.com)
 All rights reserved.
+
+This file is part of AdiSpellHUD.
+
+AdiSpellHUD is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+AdiSpellHUD is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AdiSpellHUD.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 local _G = _G
