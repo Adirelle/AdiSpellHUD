2d6da97c313421185fd62e8536dddf9a3fb44b6c
Adirelle <adirelle@gmail.com>
2013-11-19 21:37:26 +0100

    Refactored the spell overlay module to get rid of AceEvent, AceHook and hopefully errors.

--- modules/SpellOverlay.lua
+++ modules/SpellOverlay.lua
@@ -29,7 +29,7 @@ local huge, sin, pi = _G.math.huge, _G.math.sin, _G.math.pi
 local addonName, addon = ...
 local L = addon.L
 
-local mod = addon:NewModule("SpellOverlay", "AceEvent-3.0", "AceHook-3.0", "LibMovable-1.0")
+local mod = addon:NewModule("SpellOverlay", "LibMovable-1.0")
 
 local frame = _G.SpellActivationOverlayFrame
 
@@ -53,32 +53,22 @@ function mod:OnInitialize()
 	end)
 	self:SetMovable(frame, false)
 	frame:SetClampedToScreen(true)
-
-	self.throttle = 0
-	self.num = 0
 end
 
 function mod:OnEnable()
-	self:RawHook('SpellActivationOverlay_CreateOverlay', true)
-	self:RegisterEvent('UNIT_AURA')
-
-	self:UpdateOverlaysInUse()
+	if not self.Orig_SpellActivationOverlay_CreateOverlay then
+		self.Orig_SpellActivationOverlay_CreateOverlay = _G.SpellActivationOverlay_CreateOverlay
+		_G.SpellActivationOverlay_CreateOverlay = function(...)
+			if self:IsEnabled() then
+				self:Debug('SpellActivationOverlay_CreateOverlay', ...)
+				return self:SpellActivationOverlay_CreateOverlay(...)
+			else
+				return self.Orig_SpellActivationOverlay_CreateOverlay(...)
+			end
+		end
+		self:Debug('Hooked')
+	end
 end
-
---@debug@
-function _G.sotest()
-	_G.SpellActivationOverlay_OnEvent(frame, "SPELL_ACTIVATION_OVERLAY_SHOW",
-		16870, -- spellID
-		"Textures\\SpellActivationOverlays\\Natures_Grace.BLP", -- texture
-		"Left + Right (Flipped)", -- positions
-		1, -- scale,
-		255, 255, 255 -- r, g, b
-	)
-	_G.LibStub('AceTimer-3.0').ScheduleTimer(mod, function()
-		_G.SpellActivationOverlay_OnEvent(frame, "SPELL_ACTIVATION_OVERLAY_HIDE", 16870)
-	end, 8)
-end
---@end-debug@
 
 function mod:GetOptions()
 	return {
@@ -101,173 +91,184 @@ function mod:GetOptions()
 end
 
 local NOOP = function() end
-local fakePulse = { Play = NOOP, Pause = NOOP, Stop = NOOP }
 
-local i = 0
+local overlayProto = setmetatable({}, { __index = CreateFrame("Frame") })
+local overlayMeta = { __index = overlayProto }
+
+overlayProto.Debug = addon.Debug
+
+local serial = 0
 function mod:SpellActivationOverlay_CreateOverlay(parent)
-	local overlay = CreateFrame("Frame", nil, parent)
-	overlay:Hide()
-
-	overlay:SetScript('OnShow', mod.Overlay_OnShow)
-	overlay:SetScript('OnHide', mod.Overlay_OnHide)
-
-	local texture = overlay:CreateTexture(nil, "ARTWORK")
-	overlay.texture = texture
-
-	local text = overlay:CreateFontString(nil, "OVERLAY")
-	text:SetFont([[Fonts\FRIZQT__.TTF]], 24, "THICKOUTLINE")
-	text:SetAllPoints(overlay)
-	text:SetJustifyH("CENTER")
-	text:SetJustifyV("MIDDLE")
-	text:Hide()
-	overlay.text = text
-
-	overlay.pulse = fakePulse
-	overlay.animIn = { overlay = overlay, Play = mod.AnimIn_Play }
-	overlay.animOut = { overlay = overlay, Play = mod.AnimOut_Play, Stop = mod.AnimOut_Stop }
-
+	local overlay = setmetatable(CreateFrame("Frame", self.name.."SpellOverlay"..i, parent), overlayMeta)
+	serial = serial + 1
+	overlay:Initialize()
 	return overlay
 end
 
-function mod:UNIT_AURA(_, unit)
-	if unit == "player" then
-		self:UpdateOverlaysInUse()
-	end
+function overlayProto:Initialize()
+	self:Hide()
+
+	self:SetScript('OnShow', self.OnShow)
+	self:SetScript('OnHide', self.OnHide)
+	self:SetScript('OnEvent', self.OnEvent)
+	self:SetScript('OnUpdate', self.OnUpdate)
+	self:RegisterUnitEvent('UNIT_AURA', "player")
+
+	self.expires, self.duration, self.count = 0, 0, 0
+
+	local texture = self:CreateTexture(nil, "ARTWORK")
+	self.texture = texture
+
+	local text = self:CreateFontString(nil, "OVERLAY")
+	text:SetFont([[Fonts\FRIZQT__.TTF]], 24, "THICKOUTLINE")
+	text:SetAllPoints(self)
+	text:SetJustifyH("CENTER")
+	text:SetJustifyV("MIDDLE")
+	text:Hide()
+	self.text = text
+
+	-- Fake the animations of SpellActivationOverlayTemplate.
+	-- They will be used by Blizzard legacy code.
+	self.animIn = {
+		Play = function() self:SetAnimation("in") end,
+		Stop = function() self:SetAnimation(nil) end,
+		Pause = NOOP,
+	}
+	self.pulse = {
+		Play = function() self:SetAnimation("pulse") end,
+		Stop = self.animIn.Stop,
+		Pause = NOOP,
+	}
+	self.animOut = {
+		Play = function() self:SetAnimation("out") end,
+		Stop = self.animIn.Stop,
+		Pause = NOOP,
+	}
 end
 
-local seen = {}
-function mod:UpdateOverlaysInUse()
-	local overlays = frame.overlaysInUse
-	wipe(seen)
-	for index = 1, huge do
-		local name, _, _, count, _, duration, expires, _, _, _, spellID = UnitBuff("player", index)
-		if not name then break end
-		if spellID then
-			seen[spellID] = true
-			local overlays = frame.overlaysInUse[spellID]
-			if overlays then
-				for i, overlay in pairs(overlays) do
-					self:UpdateOverlay(overlay, duration, expires, count)
+function overlayProto:Update()
+	if mod:IsEnabled() then
+		for index = 1, 128 do
+			local name, _, _, count, _, duration, expires, _, _, _, spellID = UnitBuff("player", index)
+			if name then
+				if spellID == self.spellID then
+					self:SetAuraInfo(duration, expires, count)
+					return
 				end
+			else
+				break
 			end
 		end
 	end
-	for spellID, overlays in pairs(frame.overlaysInUse) do
-		if not seen[spellID]then
-			for i, overlay in pairs(overlays) do
-				self:UpdateOverlay(overlay)
-			end
-		end
-	end
+	self:SetAuraInfo(0, 0, 0)
 end
+overlayProto.UNIT_AURA = overlayProto.Update
 
-function mod:UpdateOverlay(overlay, duration, expires, count)
-	if not overlay.text then return end
-	if duration and duration > 0 then
-		overlay.expires = expires
-		overlay.duration = duration
-		overlay.text:Show()
+function overlayProto:SetAuraInfo(duration, expires, count)
+	duration, expires, count = duration or 0, expires or 0, count or 0
+	if self.duration == duration and self.expires == expires and self.count == count then return end
+	self.textTimer, self.duration, self.expires, self.count = 0, duration, expires, count
+	if duration > 0 then
+		self.text:Show()
 	else
-		overlay.text:Hide()
+		self.text:Hide()
 	end
 end
 
-function mod.OnUpdate(_, elapsed)
-	elapsed = elapsed + mod.throttle
-	local num = mod.num + 1
-	if elapsed < 0.1 and num < 2 then
-		mod.throttle, mod.num = elapsed, num
-		return
+function overlayProto:SetTextureScale(scale)
+	if self.textureScale == scale then return end
+	self.textureScale = scale
+	local w, h = self:GetSize()
+	self.texture:SetSize(scale * w, scale * h)
+end
+
+local animations = {
+	["in"] = {
+		duration = 0.3,
+		Update = function(self, progress)
+			self:SetAlpha(progress)
+			self:SetTextureScale(0.5 + progress / 2)
+		end,
+		Finished = function(self)
+			self:SetAnimation("pulse")
+		end,
+	},
+	["pulse"] = {
+		duration = 0.5,
+		Update = function(self, progress)
+			self:SetTextureScale(1 + 0.05 * sin(progress * 2 * pi))
+		end,
+		Finished = function(self)
+			self:SetAnimation("pulse")
+		end,
+	},
+	["out"] = {
+		duration = 0.3,
+		Update = function(self, progress)
+			self:SetAlpha(1 - progress)
+			self:SetTextureScale(1 + progress)
+		end,
+		Finished = function(self)
+			return self:Hide()
+		end,
+	}
+}
+
+function overlayProto:SetAnimation(animName)
+	if self.animName == animName then return end
+	self:SetAlpha(1)
+	self:SetTextureScale(1)
+	self.animName, self.animation, self.timer = animName, animName and animations[animName], 0
+end
+
+function overlayProto:OnEvent(event, ...)
+	if self:IsVisible() and mod:IsEnabled() then
+		return self[event](self, event, ...)
 	end
-	mod.throttle, mod.num = 0, 0
-	if not next(frame.overlaysInUse) then
-		frame:SetScript('OnUpdate', nil)
-	else
-		for spell, overlays in pairs(frame.overlaysInUse) do
-			for _, overlay in pairs(overlays) do
-				mod.Overlay_OnUpdate(overlay, elapsed)
+end
+
+function overlayProto:OnShow()
+	self:Debug('OnShow')
+	local point = self:GetPoint()
+	self.texture:ClearAllPoints()
+	self.texture:SetPoint(point, self, point, 0, 0)
+	self:SetAnimation("in")
+	self:Update()
+end
+
+function overlayProto:OnHide()
+	self:Debug('OnHide')
+	self:SetAnimation(nil)
+	tDeleteItem(frame.overlaysInUse[self.spellID], self)
+	tinsert(frame.unusedOverlays, self);
+end
+
+function overlayProto:OnUpdate(elapsed)
+	local anim = self.animation
+	if anim then
+		self.timer = self.timer + elapsed
+		local progress = self.timer / anim.duration
+		if progress >= 1 then
+			self:SetAnimation(nil)
+			anim.Finished(self)
+		else
+			anim.Update(self, progress)
+		end
+	end
+
+	if self.text:IsVisible() and self.expires > 0 then
+		self.textTimer = self.textTimer - elapsed
+		if self.textTimer < 0 then
+			local timeleft = self.expires - GetTime()
+			if timeleft <= 3 then
+				self.text:SetFormattedText("%3.1f", timeleft)
+				self.textTimer = timeleft % 0.1
+			else
+				self.text:SetFormattedText("%d", timeleft)
+				self.textTimer = timeleft % 1
 			end
+			local f = timeleft / self.duration
+			self.text:SetTextColor(1, min(2*f, 1), max(2*f-1, 0))
 		end
 	end
 end
-
-function mod.Overlay_OnUpdate(overlay, time)
-	time = time + overlay.time
-	overlay.time = time
-
-	local alpha, scale = 1, 1
-	if overlay.phase == 1 then
-		alpha = min(time / 0.3, 1)
-		if alpha < 1 then
-			scale = 0.5 + 0.5 * alpha
-		else
-			overlay.phase = 2
-		end
-	elseif overlay.phase == 2 then
-		scale = 1 + 0.05 * sin(time * 2 * pi)
-	elseif overlay.phase == 3 then
-		alpha = max(1 - time / 0.3, 0)
-		if alpha > 0 then
-			scale = 2 - alpha
-		else
-			overlay:Hide()
-			return
-		end
-	end
-	overlay:SetAlpha(alpha)
-	local w, h = overlay:GetSize()
-	overlay.texture:SetSize(scale * w, scale * h)
-
-	local text = overlay.text
-	if text:IsVisible() then
-		local timeleft = overlay.expires - GetTime()
-		if timeleft <= 3 then
-			text:SetFormattedText("%3.1f", timeleft)
-		else
-			text:SetFormattedText("%d", timeleft)
-		end
-		local f = timeleft / overlay.duration
-		text:SetTextColor(1, min(2*f, 1), max(2*f-1, 0))
-	end
-end
-
-function mod.Overlay_OnShow(overlay)
-	mod:Debug('Overlay_OnShow', overlay)
-	frame:SetScript('OnUpdate', mod.OnUpdate)
-	local point = overlay:GetPoint()
-	overlay.texture:ClearAllPoints()
-	overlay.texture:SetPoint(point, overlay, point, 0, 0)
-	overlay.animIn:Play()
-	mod.Overlay_OnUpdate(overlay, 0)
-end
-
-function mod.Overlay_OnHide(overlay)
-	mod:Debug('Overlay_OnHide', overlay)
-	tDeleteItem(frame.overlaysInUse[overlay.spellID], overlay)
-	tinsert(frame.unusedOverlays, overlay);
-end
-
-function mod.AnimIn_Play(anim)
-	mod:Debug('AnimIn_Play', anim)
-	local overlay = anim.overlay
-	overlay.phase = 1
-	overlay.time = 0
-end
-
-function mod.AnimOut_Play(anim)
-	mod:Debug('AnimOut_Play', anim)
-	local overlay = anim.overlay
-	overlay.text:Hide()
-	overlay.phase = 3
-	overlay.time = 0
-end
-
-function mod.AnimOut_Stop(anim)
-	local overlay = anim.overlay
-	if overlay.phase ~= 3 then return end
-	mod:Debug('AnimOut_Stop', anim)
-	overlay.text:Show()
-	overlay.phase = 2
-	overlay.time = 0
-end
-
